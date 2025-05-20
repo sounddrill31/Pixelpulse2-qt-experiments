440c6a5910cef76949f9ec537a1409cf0b22955b
SoundDrill 31 <sounddrill31@gmail.com>
2025-05-20 17:15:04 +0000

    libsmu: Allow a lower minimum cmake policy version

--- CI/prepare-build-libsmu.sh
+++ CI/prepare-build-libsmu.sh
@@ -4,7 +4,7 @@ sudo rm -rf libsmu
 git clone https://github.com/sounddrill31/libsmu.git
 cd libsmu
 mkdir build && cd build
-cmake -DBUILD_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr/ ..
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=2.8 -DBUILD_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr/ ..
 make
 sudo make install
 cd ../..
