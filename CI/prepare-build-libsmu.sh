2d1375686b24091c82b15698982ae940f1c70f5a
SoundDrill 31 <sounddrill31@gmail.com>
2025-05-20 17:18:54 +0000

    Raise minimum cmake version requirement

--- CI/prepare-build-libsmu.sh
+++ CI/prepare-build-libsmu.sh
@@ -4,7 +4,7 @@ sudo rm -rf libsmu
 git clone https://github.com/sounddrill31/libsmu.git
 cd libsmu
 mkdir build && cd build
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=2.8 -DBUILD_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr/ ..
+cmake -DBUILD_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr/ ..
 make
 sudo make install
 cd ../..
